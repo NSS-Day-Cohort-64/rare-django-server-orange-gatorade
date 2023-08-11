bed553cc9724797945138c26af4892509f1b3232
Ryan Phillips <ryanmphill@gmail.com>
2023-08-11 14:29:35 -0500

    Updated .gitignore

--- seed_database.sh
+++ seed_database.sh
@@ -7,3 +7,4 @@ python3 manage.py makemigrations gatoradeapi
 python3 manage.py migrate gatoradeapi
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
+python3 manage.py loaddata authors
