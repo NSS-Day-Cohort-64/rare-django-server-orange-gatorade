d2df4441209b1a928a3e31e8f4014cf7f0207f28
Ryan Phillips <ryanmphill@gmail.com>
2023-08-11 13:41:19 -0500

    Initial Django Setup

---
+++ seed_database.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./gatoradeapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations gatoradeapi
+python3 manage.py migrate gatoradeapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
