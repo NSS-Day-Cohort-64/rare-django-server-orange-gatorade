6925291ec6062475cf3210000ce2b5564f0b0cbf
Ryan Phillips <ryanmphill@gmail.com>
2023-08-14 11:33:50 -0500

    Added auth view with logic for login and register and added routing to urls.py

--- gatorade/urls.py
+++ gatorade/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
@@ -26,6 +27,8 @@ router.register(r'tags', TagView, 'tag')
 router.register(r'reactions', ReactionView, 'reaction')
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
     path('', include(router.urls))
 ]
