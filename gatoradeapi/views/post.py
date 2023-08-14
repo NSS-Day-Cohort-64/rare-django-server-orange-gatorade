92fd1464dfac72ddc0a248937af6b9cde89c1c64
Lance Buckley <erchancy@gmail.com>
2023-08-14 11:17:52 -0500

    Added list, retrieve, create, update, delete, and several list filters such as author, category, approved, and tags. Altered the post model reactions fk to reference reactions and the datefield to set auto_now_add to true

--- gatoradeapi/views/post.py
+++ gatoradeapi/views/post.py
@@ -1,5 +1,5 @@
 """View module for handling requests about Posts"""
-from django.http import HttpResponseServerError
+from django.db.models import Q
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
@@ -17,8 +17,84 @@ class PostView(ViewSet):
         """
 
         post = Post.objects.all()
+        if "approved" in request.query_params:
+            post = post.filter(approved=True)
+        if "author" in request.query_params:
+            post = post.filter(author=request.query_params['author'][0])
+        if "category" in request.query_params:
+            post = post.filter(category=request.query_params['category'][0])
+        # Handle filtering based on tags
+        if "tag" in request.query_params:
+            tag_ids = request.query_params.getlist('tag')
+            q_objects = Q()
+            for tag_id in tag_ids:
+                q_objects |= Q(tags=tag_id)
+            post = post.filter(q_objects)
         serializer = PostSerializer(post, many=True)
         return Response(serializer.data)
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for a single Post
+
+        Returns:
+            Response -- JSON serialized Post
+        """
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized Post instance
+        """
+        author = Author.objects.get(pk=request.data["author"])
+        category = Category.objects.get(pk=request.data["category"])
+        tags = Tag.objects.filter(pk__in=request.data["tags"])
+
+        post = Post.objects.create(
+            title=request.data['title'],
+            author=author,
+            category=category,
+            image_url=request.data['image_url'],
+            content=request.data['content'],
+            approved=False,
+        )
+
+        post.tags.set(tags)
+
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a Post
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        post = Post.objects.get(pk=pk)
+        tags = Tag.objects.filter(pk__in=request.data["tags"])
+        post.title = request.data["title"]
+        post.author = Author.objects.get(pk=request.data["author"])
+        post.category = Category.objects.get(pk=request.data["category"])
+        post.image_url = request.data["image_url"]
+        post.content = request.data["content"]
+        post.tags.set(tags)
+        post.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        """Handle DELETE requests for a Post
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -51,6 +127,7 @@ class PostSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(many=False)
     category = CategorySerializer(many=False)
     tags = TagSerializer(many=True)
+    user_reactions = UserReactionSerializer(many=True)
 
     class Meta:
         model = Post
